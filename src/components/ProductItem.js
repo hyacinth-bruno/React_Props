10a4d7d8a6a7bf899a87e55c40912f23410530c1
hyacinth-bruno <mbachubruno.chidozie@gmail.com>
2023-07-18 12:42:26 +0200

    Improved Commit
    🛍️ ProductList App: A sleek and responsive web application built with React.js, showcasing a list of products with their details in an engaging grid format. Features include CSS styling and image hover effects, with a design optimized for various screen sizes.

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,11 +1,26 @@
+// import React from "react";
+
+// export default function ProductItem({ product }) {
+//   return (
+//     <div class="product">
+//      <div>Name: {product.title}</div>  <br />
+//      <div>Price: {product.price}</div>  <br />
+//      <div><img src={product.image} width="60px"/></div> 
+//     </div>
+//   );
+// }
+
 import React from "react";
 
-export default function ProductItem({ product }) {
+function ProductItem({product}) {
   return (
-    <div class="product">
-     <div>Name: {product.title}</div>  <br />
-     <div>Price: {product.price}</div>  <br />
-     <div><img src={product.image} width="60px"/></div> 
+    <div className="product-item">
+      <img src={product.image} alt={product.title}/>
+      <h2>{product.title}</h2>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
     </div>
   );
 }
+
+export default ProductItem;
